193a47a088c3d87a20a16afd5894305e2d4eca95
Alegarciy <41702000+Alegarciy@users.noreply.github.com>
2021-03-26 14:49:41 -0600

    U: Jerarquia Padre Hijo Hermanos

--- Angular_curso_udemy/MiPrimerApp/src/app/app.module.ts
+++ Angular_curso_udemy/MiPrimerApp/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './saludo/saludo.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
+import { Hijo2Component } from './hijo2/hijo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludoComponent
+    SaludoComponent,
+    PadreComponent,
+    HijoComponent,
+    Hijo2Component
   ],
   imports: [
     BrowserModule
